b977918cbde2f5d4e5a802a1d10d60ad785fbfd2
IgorHamzi <igorwhamzi@gmail.com>
2021-11-01 15:49:29 -0300

    adiciona o requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <section>
+          {
+            planets.map((planet) => {
+              const { name, image } = planet;
+              return (<PlanetCard
+                key={ planet.name }
+                planetName={ name }
+                planetImage={ image }
+              />);
+            })
+          }
+        </section>
       </div>
     );
   }
